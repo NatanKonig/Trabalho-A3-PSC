fdefe8f70da1ea5df0869bb352945309dd931755
Gabriel <gmdf10@gmail.com>
2025-05-16 19:06:15 -0300

    declaração dos atributos e seus gets e setters

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,4 @@
-package com.psc;
+package app;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/model/Produto.java
@@ -0,0 +1,94 @@
+package model;
+
+import com.psc.model.Categoria;
+
+public class Produto {
+int id;
+String nome;
+double precoUnitario;
+String unidade;
+int quantidadeEstoque;
+int quantidadeMinima;
+int quantidadeMaxima;
+Categoria categoria;
+
+public Produto() {}
+
+    public Produto(int id, String nome, double precoUnitario, String unidade, int quantidadeEstoque, int quantidadeMinima, int quantidadeMaxima, Categoria categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.precoUnitario = precoUnitario;
+        this.unidade = unidade;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.quantidadeMinima = quantidadeMinima;
+        this.quantidadeMaxima = quantidadeMaxima;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public int getQuantidadeMinima() {
+        return quantidadeMinima;
+    }
+
+    public void setQuantidadeMinima(int quantidadeMinima) {
+        this.quantidadeMinima = quantidadeMinima;
+    }
+
+    public int getQuantidadeMaxima() {
+        return quantidadeMaxima;
+    }
+
+    public void setQuantidadeMaxima(int quantidadeMaxima) {
+        this.quantidadeMaxima = quantidadeMaxima;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+
+
+}
