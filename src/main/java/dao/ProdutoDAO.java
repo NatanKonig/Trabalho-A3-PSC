23e37323539cd04b84795347a62b6f08ebee127c
Gabriel <gmdf10@gmail.com>
2025-05-17 15:12:37 -0300

    DAO

--- src/main/java/dao/ProdutoDAO.java
+++ src/main/java/dao/ProdutoDAO.java
@@ -1,4 +1,17 @@
 package dao;
 
+import model.Produto;
+
+import java.util.ArrayList;
+
 public class ProdutoDAO {
+    private ArrayList<Produto> lista = new ArrayList<Produto>();
+
+    public void inserir(Produto produto) {
+        lista.add(produto);
+    }
+
+    public ArrayList<Produto> listar() {
+        return lista;
+    }
 }
