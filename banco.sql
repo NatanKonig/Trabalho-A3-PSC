66fc8ccd08d83cce4469f31d111652183ae1ad4a
mario <mario.imperator06@gmail.com>
2025-05-27 21:03:44 -0300

    feat: atualização em banco.sql

--- banco.sql
+++ banco.sql
@@ -18,13 +18,3 @@ CREATE TABLE produto (
     PRIMARY KEY (id_produto),
     FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria)
 );
-
-CREATE TABLE movimentacao (
-    id_movimentacao INT NOT NULL AUTO_INCREMENT,
-    id_produto INT,
-    tipo ENUM('Entrada', 'Saida') NOT NULL,
-    quantidade INT NOT NULL,
-    PRIMARY KEY (id_movimentacao),
-    data DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_produto) REFERENCES produto(id_produto)
-);
