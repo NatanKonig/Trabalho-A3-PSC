0ab2ba054e6928cfc36d9722e4b358f4d5cfaa85
Fabricio de Aguiar <Fabricio de Aguiar@Josfino>
2025-05-29 17:51:12 -0300

    Revert "Revert "Revert "feat: adicionado conexaoDAO com config.properties"""
    This reverts commit 3fd5951145f9da9b56c0445f3790cc6a9389865f.

--- src/main/java/com/psc/dao/ConexaoDAO.java
+++ src/main/java/com/psc/dao/ConexaoDAO.java
@@ -1,37 +1,22 @@
 package com.psc.dao;
 
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
-
+/**
+ *
+ * @author mario
+ */
 public class ConexaoDAO {
-    private static String URL;
-    private static String USER;
-    private static String PASSWORD;
-
-    static {
+    public Connection conectaBD() {
+        Connection conn = null;
         try {
-            Properties props = new Properties();
-            InputStream input = ConexaoDAO.class.getClassLoader().getResourceAsStream("config.properties");
-
-            if (input == null) {
-                throw new RuntimeException("Arquivo config.properties não encontrado.");
-            }
-
-            props.load(input);
-            URL = props.getProperty("db.url");
-            USER = props.getProperty("db.user");
-            PASSWORD = props.getProperty("db.password");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Erro ao carregar configurações do banco.", e);
+            String url = "jdbc:mysql://localhost:3306/seu_banco?user=root&password=sua_senha";
+            conn = DriverManager.getConnection(url);
+            System.out.println("Conexão realizada com sucesso.");
+        } catch (SQLException e) {
+            System.out.println("Erro na conexão: " + e.getMessage());
         }
-    }
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return conn;
     }
 }
